057ec1fa98a8cdba12cd45b1d27b1bfbcca28ca8
Farukul Islam <farukgb1999@gmail.com>
2023-05-02 12:30:08 +0600

    data problem fixed

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+
+const chefs = require("./data/chefData.json")
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get("/chefs",(req,res)=>{
+    res.send(chefs);
+})
+app.get("/chefs/:id", (req, res) => {
+    const chefsId = req.params.id;
+    const singleChefs = chefs.find((getChefs) => getChefs.id == chefsId);
+    res.send(singleChefs);
+  });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
